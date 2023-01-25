1cea0ef6f42f4dc92c7bd8b5136e80a89e50c1b5
Maahir Vohra <Maahir Vohra>
2023-01-24 22:23:34 -0500

    hw1

--- HW1_MaahirVohra.py
+++ HW1_MaahirVohra.py
@@ -1,1 +1,19 @@
-print("Maahir")
+#Maahir Vohra
+#CS100 Section 008
+#HW 01, January 20, 2023
+
+#b
+numTires = 4
+numHours = 24
+angle = 90
+
+#c
+weight = 150.50
+pi = 3.14159
+fahrenheit = 70.9
+
+#d
+studentName = "Maahir"
+schoolName = "NJIT"
+schoolLocation = "Newark"
+
