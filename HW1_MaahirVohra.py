334166c4606011a3f45a9ae2d19571c66cd0198a
Maahir Vohra <Maahir Vohra>
2023-01-26 18:05:57 -0500

    hw1

--- HW1_MaahirVohra.py
+++ HW1_MaahirVohra.py
@@ -17,3 +17,81 @@ studentName = "Maahir"
 schoolName = "NJIT"
 schoolLocation = "Newark"
 
+#1.1
+'''
+#1
+If you leave out 1 or both parenthesis you get a syntax error.
+Ex) SyntaxError: '(' was never closed
+
+#2
+If you leave out 1 quotation mark or both you'll get a syntax error.
+If you want to print a variable out, you can use the print statement without any quotes.
+
+#3
+Putting a plus sign before a positive number doesn't change anything
+Doing 2++2 doesn't change the addition, but you may get an error
+
+#4
+Putting a leading zero will cause a syntax error. 
+Leading zeros in decimal integer literals are not permitted
+
+#5
+You will get a syntax error. You can't put two numbers without a operator it will just give you invalid syntax
+
+'''
+
+#1.2
+'''
+#1
+To calculate how many seconds there are, you do (minutes * 60) + seconds.
+In this case it would be (42 * 60) + 42 = 2562 seconds
+
+#2
+To convert kilometers to miles. You do (kilometers / 1.61)
+In this case it would be (10 / 1.61) = 6.21 miles
+
+#3
+
+'''
+
+#2.1
+'''
+#1
+42 = n is not legal. The variable is supposed to come first. You will get a syntax error.
+
+#2
+Yes, it is legal. You are setting both x and y = 1
+
+#3
+The program will still run on VSCode if you put a semicolon at the end of a python statement.
+You may get a syntax error because a semicolon does not go there.
+
+#4
+You get a syntax error saying invalid syntax
+
+#5
+You will get a Name Error. If you put x and y together with no spaces, python will look for the variable xy instead of doing x * y.
+
+'''
+
+#2.2
+'''
+#1
+If volume = (4/3) * pi * r^3
+r = 5, then volume = (4/3) * pi * 5^3
+therefore, volume = 523.6
+
+#2
+cover price = 24.95
+bookStorePrice = 14.97
+shipping = 3 + 44.25 = 47.25
+total cost = (bookStorePrice * 60) + shipping = $945.45
+
+#3
+easyPace = 8:15
+tempo = 7:12
+startTime = 6:52
+So, startTime + easyPace + (3 * tempo) + easyPace
+You will get home at 7:50 and 6 seconds for breakfast
+'''
+
