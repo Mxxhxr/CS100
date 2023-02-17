3d77ae32c0d778c55a04f7b67325c3dcd96044cf
Maahir Vohra <Maahir Vohra>
2023-02-16 23:36:21 -0500

    hw5DONE

--- HW5_MaahirVohra.py
+++ HW5_MaahirVohra.py
@@ -1,9 +1,29 @@
 #Maahir Vohra
 #CS100 Section 008
-#HW 04, February 7, 2023
+#HW 05, February 16, 2023
 
 
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+horton = "A person's a person, no matter how small."
+vowels = "aeiouAEIOU"
 
-for x in months:
-    
+
+
+##1
+for letter in months:
+    firstLetter = letter[0][0]
+    if firstLetter == 'J':
+        print(letter)
+
+
+##2
+for i in range(100):
+    if i % 2 == 0 and i % 5 == 0:
+        print(i)
+
+
+##3
+for x in horton:
+    if x in vowels:
+        print(x)
+
