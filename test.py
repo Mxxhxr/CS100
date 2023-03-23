4cc09939441788ad1cfd192cfba440d9216bcc64
Maahir Vohra <Maahir Vohra>
2023-03-23 17:21:56 -0400

    hw7

--- test.py
+++ test.py
@@ -1,10 +1,51 @@
-start = 1
-stop = 10
-step = 1
+def print_board(board):
+    print("   |   |")
+    print(" " + board[0] + " | " + board[1] + " | " + board[2])
+    print("___|___|___")
+    print("   |   |")
+    print(" " + board[3] + " | " + board[4] + " | " + board[5])
+    print("___|___|___")
+    print("   |   |")
+    print(" " + board[6] + " | " + board[7] + " | " + board[8])
+    print("   |   |")
 
 
-for num in range(start, stop, step):
-    if num != stop - stop:
-        print(num, end=",")
+def check_win(board, player):
+    if (board[0] == player and board[1] == player and board[2] == player) or \
+       (board[3] == player and board[4] == player and board[5] == player) or \
+       (board[6] == player and board[7] == player and board[8] == player) or \
+       (board[0] == player and board[3] == player and board[6] == player) or \
+       (board[1] == player and board[4] == player and board[7] == player) or \
+       (board[2] == player and board[5] == player and board[8] == player) or \
+       (board[0] == player and board[4] == player and board[8] == player) or \
+       (board[2] == player and board[4] == player and board[6] == player):
+        return True
     else:
-        print(num)
+        return False
+
+
+def play_game():
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    player = "X"
+    while True:
+        print_board(board)
+        move = input("It's " + player + "'s turn. Enter a position (1-9): ")
+        if board[int(move)-1] != " ":
+            print("That position is already taken. Please try again.")
+            continue
+        board[int(move)-1] = player
+        if check_win(board, player):
+            print_board(board)
+            print("Congratulations, " + player + " wins!")
+            break
+        if " " not in board:
+            print_board(board)
+            print("It's a tie!")
+            break
+        if player == "X":
+            player = "O"
+        else:
+            player = "X"
+
+
+play_game()
